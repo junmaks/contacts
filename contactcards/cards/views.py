a0f2d3c8bea4fe07b008c1ffade9f5ed0a99ad02
Maksim <maxmiassspb@gmail.com>
2021-02-02 08:54:53 +0300

    Correcting add_contact, information, edit_contact - add new form (PhoneNumber)

--- contactcards/cards/views.py
+++ contactcards/cards/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import ContactCards
-from .forms import CardForm
+from .models import ContactCards, PhoneNumberModel
+from .forms import CardForm, PhoneNumberForm
 
 
 # Create your views here.
@@ -21,37 +21,55 @@ def contacts(request):
 
 def info_contact(request, contact_id):
     contact = get_object_or_404(ContactCards, pk=contact_id)  # возвращает 404, если объект модели не найден
-    return render(request=request, template_name='cards/information.html', context={'contact': contact})
+    phone_numbers = PhoneNumberModel.objects.filter(id_user=contact_id)
+    print(phone_numbers)
+    return render(request=request, template_name='cards/information.html', context={'contact': contact,
+                                                                                    'phone_numbers': phone_numbers})
 
 
 def add_contact(request, ):
     if request.method == 'POST':
-        form = CardForm(request.POST)
-        if form.is_valid():
-            card = form.save()
+        form1 = CardForm(request.POST)
+        form2 = PhoneNumberForm(request.POST)
+        if form1.is_valid() and form2.is_valid():
+            print(form1.cleaned_data)
+            card = form1.save()
+            print(type(card))
+            print(card.pk)
+
+            # id_user = card.pk
+            phone_number = form2.cleaned_data['phone_number']
+            PhoneNumberModel.objects.create(id_user=card, phone_number=phone_number)
+
             return redirect(card)
     else:
-        form = CardForm()
+        form1 = CardForm()
+        form2 = PhoneNumberForm()
     context = {
-        'form': form
+        'form1': form1,
+        'form2': form2,
     }
     return render(request=request, template_name='cards/add_contact.html', context=context)
 
 
 def edit_contact(request, contact_id):
-    print(__name__)
+    # print(__name__)
     contact = get_object_or_404(ContactCards, pk=contact_id)
-    print(contact.name)
-    print(request.method)
+    phone = PhoneNumberModel.objects.get(id_user=contact_id)
+    # print(contact.name)
+    # print(request.method)
     if request.method == 'POST':
-        form = CardForm(request.POST, instance=contact)
+        form1 = CardForm(request.POST, instance=contact)
+        form2 = PhoneNumberForm(request.POST, instance=phone)
         print(contact.surname)
-        if form.is_valid():
-            card = form.save()
+        if form1.is_valid():
+            card = form1.save()
             return redirect(card, pk=contact_id)
     else:
-        form = CardForm(instance=contact)
+        form1 = CardForm(instance=contact)
+        form2 = PhoneNumberForm(instance=phone)
     context = {
-        'form': form
+        'form1': form1,
+        'form2': form2
     }
     return render(request=request, template_name='cards/add_contact.html', context=context)
